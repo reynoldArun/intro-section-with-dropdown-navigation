1b4bd05e32b6922a3f0d09dec7ece65a3d2677a1
reynoldArun <reynold.seq@gmail.com>
2022-12-22 07:53:58 +0530

    dropdown

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 const menuEl = document.querySelector('#menu')
 const openEl = document.querySelector('#open')
 const closeEl = document.querySelector('#close')
+const featuresEl = document.querySelector('#features')
+const companyEl = document.querySelector('#company')
+const hideEl = document.querySelector('#hide')
+const hide1El = document.querySelector('#hide1')
+
+
+featuresEl.addEventListener('click', () => {
+    if(hideEl.classList.contains('hide')) {
+        hideEl.classList.remove('hide')
+   } else {
+        hideEl.classList.add('hide')
+   }
+})
+
+companyEl.addEventListener('click', () => {
+    if(hide1El.classList.contains('hide1')) {
+        hide1El.classList.remove('hide1')
+   } else {
+        hide1El.classList.add('hide1')
+   }
+})
 
 
 menuEl.addEventListener('click', () => {
